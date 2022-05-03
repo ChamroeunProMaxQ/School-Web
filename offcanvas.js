940ce5e088979808f7b3dc23ad2f1005f436111d
ChamroeunProMaxQ <stevenkakoreal@gmail.com>
2022-05-03 12:56:11 +0700

    first commit

---
+++ offcanvas.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
+
+
+
+
+
